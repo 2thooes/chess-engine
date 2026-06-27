69f4ee22a14d3cd5d1154a44a4a0bbc3fe917f80
2thooes <2thooes@gmail.com>
2026-06-27 19:54:59 -0400

    modified main

--- main.c
+++ main.c
@@ -7,8 +7,7 @@
 
 #include <unistd.h>
 #include <sys/types.h>
-typedef struct
-{
+typedef struct{
     uint64_t whitePawns;
     uint64_t whiteKnights;
     uint64_t whiteBishops;
@@ -26,16 +25,16 @@ typedef struct
     int whiteTurn;
     int castlingRights;
     int enPassantSquare;
-
 } Board;
 
 Board board;
 
+char startPosition[200];
+
 char moveHistory[1024][6];
 int moveCount = 0;
 
-void clearBoard(Board *b)
-{
+void clearBoard(Board *b){
     b->whitePawns=0;
     b->whiteKnights=0;
     b->whiteBishops=0;
@@ -51,23 +50,16 @@ void clearBoard(Board *b)
     b->blackKing=0;
 }
 
-void initializeClassic(Board *b)
-{
+void initializeClassic(Board *b){
     clearBoard(b);
-
-
     // white
-
     b->whitePawns   = 0x000000000000FF00;
     b->whiteRooks   = 0x0000000000000081;
     b->whiteKnights = 0x0000000000000042;
     b->whiteBishops = 0x0000000000000024;
     b->whiteQueens  = 0x0000000000000008;
     b->whiteKing    = 0x0000000000000010;
-
-
     // black
-
     b->blackPawns   = 0x00FF000000000000;
     b->blackRooks   = 0x8100000000000000;
     b->blackKnights = 0x4200000000000000;
@@ -75,19 +67,89 @@ void initializeClassic(Board *b)
     b->blackQueens  = 0x0800000000000000;
     b->blackKing    = 0x1000000000000000;
 
-
     b->whiteTurn = 1;
-
     // KQkq
     b->castlingRights = 15;
-
     b->enPassantSquare = -1;
+
+    strcpy(startPosition,"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 }
 
-int occupied(Board *b, int sq)
+void createFEN(Board *b)
 {
-    uint64_t mask = 1ULL << sq;
+    char fen[200]="";
+    int empty=0;
 
+    for(int rank=7;rank>=0;rank--)
+    {
+        for(int file=0;file<8;file++)
+        {
+            int sq=rank*8+file;
+            uint64_t mask=1ULL<<sq;
+
+            char c=0;
+
+
+            if(b->whitePawns&mask)c='P';
+            else if(b->whiteKnights&mask)c='N';
+            else if(b->whiteBishops&mask)c='B';
+            else if(b->whiteRooks&mask)c='R';
+            else if(b->whiteQueens&mask)c='Q';
+            else if(b->whiteKing&mask)c='K';
+
+            else if(b->blackPawns&mask)c='p';
+            else if(b->blackKnights&mask)c='n';
+            else if(b->blackBishops&mask)c='b';
+            else if(b->blackRooks&mask)c='r';
+            else if(b->blackQueens&mask)c='q';
+            else if(b->blackKing&mask)c='k';
+
+
+
+            if(c)
+            {
+                if(empty)
+                {
+                    char e[5];
+                    sprintf(e,"%d",empty);
+                    strcat(fen,e);
+                    empty=0;
+                }
+
+                char s[2]={c,0};
+                strcat(fen,s);
+            }
+            else
+            {
+                empty++;
+            }
+        }
+
+
+        if(empty)
+        {
+            char e[5];
+            sprintf(e,"%d",empty);
+            strcat(fen,e);
+            empty=0;
+        }
+
+
+        if(rank!=0)
+            strcat(fen,"/");
+    }
+
+
+    strcat(fen, b->whiteTurn ? " w " : " b ");
+
+    strcat(fen,"- - 0 1");
+
+
+    strcpy(startPosition,fen);
+}
+
+int occupied(Board *b, int sq){
+    uint64_t mask = 1ULL << sq;
 
     return
     (b->whitePawns   & mask) ||
@@ -105,33 +167,87 @@ int occupied(Board *b, int sq)
     (b->blackKing    & mask);
 }
 
-int squareFromString(char file, char rank)
-{
+int squareFromString(char file, char rank){
     int x = file - 'a';
     int y = rank - '1';
 
     if(x < 0 || x > 7 || y < 0 || y > 7)
         return -1;
-
     return y * 8 + x;
 }
 
-int file(int sq)
-{
+int column(int sq){
     return sq % 8;
 }
 
-
-int rank(int sq)
-{
+int row(int sq){
     return sq / 8;
 }
 
-bool legitMove(char move[], bool whiteTurn)
-{
+int clearDiagonal(int from, int to){
+    int dx = column(to) - column(from);
+    int dy = row(to) - row(from);
+
+    int stepX = (dx > 0) ? 1 : -1;
+    int stepY = (dy > 0) ? 1 : -1;
+
+
+    int x = column(from) + stepX;
+    int y = row(from) + stepY;
+
+
+    while(x != column(to) && y != row(to))
+    {
+        int sq = y * 8 + x;
+
+        if(occupied(&board, sq))
+            return 0;
+
+        x += stepX;
+        y += stepY;
+    }
+
+    return 1;
+}
+
+int clearStraight(int from, int to){
+    int dx = column(to) - column(from);
+    int dy = row(to) - row(from);
+
+    int stepX = 0;
+    int stepY = 0;
+
+
+    if(dx != 0)
+        stepX = (dx > 0) ? 1 : -1;
+
+    if(dy != 0)
+        stepY = (dy > 0) ? 1 : -1;
+
+
+    int x = column(from) + stepX;
+    int y = row(from) + stepY;
+
+
+    while(x != column(to) || y != row(to))
+    {
+        int sq = y * 8 + x;
+
+        if(occupied(&board, sq))
+            return 0;
+
+        x += stepX;
+        y += stepY;
+    }
+
+
+    return 1;
+}
+
+bool legitMove(char move[], bool whiteTurn){
     if(strlen(move) != 4)
         return false;
-
+    //decoding coordinates
     int from = squareFromString(
         move[0],
         move[1]
@@ -140,18 +256,18 @@ bool legitMove(char move[], bool whiteTurn)
         move[2],
         move[3]
     );
+    //decoding coordinates
 
     if(from == -1 || to == -1)
         return false;
     uint64_t fromMask = 1ULL << from;
     uint64_t toMask   = 1ULL << to;
     /*
-        ПЕШКА
+        PAWN
     */
-    if(whiteTurn && (board.whitePawns & fromMask))
-    {
-        int dx = file(to) - file(from);
-        int dy = rank(to) - rank(from);
+    if(whiteTurn && (board.whitePawns & fromMask)){
+        int dx = column(to) - column(from);
+        int dy = row(to) - row(from);
         // обычный ход вверх
         if(dx == 0 && dy == 1)
         {
@@ -159,85 +275,359 @@ bool legitMove(char move[], bool whiteTurn)
                 return true;
         }
         // первый ход на две клетки
-        if(rank(from)==1 && dx==0 && dy==2)
+        if(row(from)==1 && dx==0 && dy==2)
         {
             int middle = from + 8;
-
-            if(!occupied(&board,middle) &&
-               !occupied(&board,to))
+            if(!occupied(&board,middle) && !occupied(&board,to))
             {
                 return true;
             }
         }
         // взятие
-        if(abs(dx)==1 &&
-           dy==1)
+        if(abs(dx)==1 && dy==1)
         {
             if(occupied(&board,to))
-                return true;
+                return true; 
         }
         return false;
     }
-
-    /*
-        ЧЕРНАЯ ПЕШКА
-    */
-    if(!whiteTurn && (board.blackPawns & fromMask))
-    {
-        int dx = file(to) - file(from);
-        int dy = rank(to) - rank(from);
+    if(!whiteTurn && (board.blackPawns & fromMask)){
+        int dx = column(to) - column(from);
+        int dy = row(to) - row(from);
         // вниз
-        if(dx==0 && dy==-1)
-        {
+        if(dx==0 && dy==-1){
             if(!occupied(&board,to))
                 return true;
         }
 
         // первый ход
-        if(rank(from)==6 &&
-           dx==0 &&
-           dy==-2)
-        {
-
+        if(row(from)==6 && dx==0 && dy==-2){
             int middle = from - 8;
 
-
-            if(!occupied(&board,middle) &&
-               !occupied(&board,to))
+            if(!occupied(&board,middle) && !occupied(&board,to))
             {
                 return true;
             }
         }
-
-
-
         // взятие
-
-        if(abs(dx)==1 &&
-           dy==-1)
-        {
+        if(abs(dx)==1 && dy==-1){
             if(occupied(&board,to))
                 return true;
         }
 
+        return false;
+    }
+    /*
+        KNIGHT
+    */
+    if(whiteTurn && (board.whiteKnights & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
 
+        if((dx == 1 && dy == 2) || (dx == 2 && dy == 1)){
+            // нельзя вставать на свою фигуру
+            uint64_t whitePieces =
+                board.whitePawns |
+                board.whiteKnights |
+                board.whiteBishops |
+                board.whiteRooks |
+                board.whiteQueens |
+                board.whiteKing;
+            if(!(whitePieces & toMask))
+                return true;
+        }
+        return false;
+    }
+    if(!whiteTurn && (board.blackKnights & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
+
+        if((dx == 1 && dy == 2) || (dx == 2 && dy == 1))
+        {
+            // нельзя вставать на свою фигуру
+            uint64_t blackPieces =
+                board.blackPawns |
+                board.blackKnights |
+                board.blackBishops |
+                board.blackRooks |
+                board.blackQueens |
+                board.blackKing;
+            if(!(blackPieces & toMask))
+                return true;
+        }
+
+        return false;
+    }
+    /*
+        BISHOP
+    */
+    if(whiteTurn && (board.whiteBishops & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
+
+        // слон ходит только по диагонали
+        if(dx == dy && dx != 0)
+        {
+            if(clearDiagonal(from,to))
+            {
+                uint64_t whitePieces =
+                    board.whitePawns |
+                    board.whiteKnights |
+                    board.whiteBishops |
+                    board.whiteRooks |
+                    board.whiteQueens |
+                    board.whiteKing;
+
+                if(!(whitePieces & toMask))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    if(!whiteTurn && (board.blackBishops & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
+
+        if(dx == dy && dx != 0){
+            if(clearDiagonal(from,to)){
+                uint64_t blackPieces =
+                    board.blackPawns |
+                    board.blackKnights |
+                    board.blackBishops |
+                    board.blackRooks |
+                    board.blackQueens |
+                    board.blackKing;
+
+                if(!(blackPieces & toMask))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+    /*
+        ROOK
+    */
+    if(whiteTurn && (board.whiteRooks & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
+
+        // движение по вертикали или горизонтали
+        if(dx == 0 || dy == 0)
+        {
+            // чтобы не разрешить стояние на месте
+            if(dx == 0 && dy == 0)
+                return false;
+
+            if(clearStraight(from,to))
+            {
+                uint64_t whitePieces =
+                    board.whitePawns |
+                    board.whiteKnights |
+                    board.whiteBishops |
+                    board.whiteRooks |
+                    board.whiteQueens |
+                    board.whiteKing;
+
+                if(!(whitePieces & toMask))
+                    return true;
+            }
+        }
 
         return false;
     }
 
+    if(!whiteTurn && (board.blackRooks & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
 
+        if(dx == 0 || dy == 0){
+            if(dx == 0 && dy == 0)
+                return false;
+
+            if(clearStraight(from,to)){
+                uint64_t blackPieces =
+                    board.blackPawns |
+                    board.blackKnights |
+                    board.blackBishops |
+                    board.blackRooks |
+                    board.blackQueens |
+                    board.blackKing;
+
+                if(!(blackPieces & toMask))
+                    return true;
+            }
+        }
+        return false;
+    }
+    /*
+        QUEEN
+    */
+    if(whiteTurn && (board.whiteQueens & fromMask)){
+    int dx = abs(column(to) - column(from));
+    int dy = abs(row(to) - row(from));
+
+
+    // диагональ или прямая
+    if((dx == dy && dx != 0) ||
+       (dx == 0 || dy == 0))
+    {
+
+        if(dx == 0 && dy == 0)
+            return false;
+
+
+        int clear = 0;
+
+
+        if(dx == dy)
+            clear = clearDiagonal(from,to);
+        else
+            clear = clearStraight(from,to);
+
+
+        if(clear)
+        {
+            uint64_t whitePieces =
+                board.whitePawns |
+                board.whiteKnights |
+                board.whiteBishops |
+                board.whiteRooks |
+                board.whiteQueens |
+                board.whiteKing;
+
+
+            if(!(whitePieces & toMask))
+                return true;
+        }
+    }
+
+    return false;
+    }
+
+    if(!whiteTurn && (board.blackQueens & fromMask)){
+    int dx = abs(column(to) - column(from));
+    int dy = abs(row(to) - row(from));
+
+
+    if((dx == dy && dx != 0) ||
+       (dx == 0 || dy == 0))
+    {
+
+        if(dx == 0 && dy == 0)
+            return false;
+
+
+        int clear;
+
+
+        if(dx == dy)
+            clear = clearDiagonal(from,to);
+        else
+            clear = clearStraight(from,to);
+
+
+        if(clear)
+        {
+            uint64_t blackPieces =
+                board.blackPawns |
+                board.blackKnights |
+                board.blackBishops |
+                board.blackRooks |
+                board.blackQueens |
+                board.blackKing;
+
+
+            if(!(blackPieces & toMask))
+                return true;
+        }
+    }
+
+    return false;
+    }
+    /*
+    KING
+    */
+    if(whiteTurn && (board.whiteKing & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
+
+
+        if(dx <= 1 && dy <= 1){
+            if(dx == 0 && dy == 0)
+                return false;
+
+            uint64_t whitePieces =
+                board.whitePawns |
+                board.whiteKnights |
+                board.whiteBishops |
+                board.whiteRooks |
+                board.whiteQueens |
+                board.whiteKing;
+
+            if(!(whitePieces & toMask))
+                return true;
+        }
+
+        return false;
+    }
+
+    if(!whiteTurn && (board.blackKing & fromMask)){
+        int dx = abs(column(to) - column(from));
+        int dy = abs(row(to) - row(from));
+
+        if(dx <= 1 && dy <= 1){
+            if(dx == 0 && dy == 0)
+                return false;
+
+            uint64_t blackPieces =
+                board.blackPawns |
+                board.blackKnights |
+                board.blackBishops |
+                board.blackRooks |
+                board.blackQueens |
+                board.blackKing;
+
+            if(!(blackPieces & toMask))
+                return true;
+        }
+
+        return false;
+    }
 
     return false;
 }
 
-void changeBoard(char move[])
-{
+void capturePiece(int sq){
+    uint64_t mask = 1ULL << sq;
+
+    board.whitePawns   &= ~mask;
+    board.whiteKnights &= ~mask;
+    board.whiteBishops &= ~mask;
+    board.whiteRooks   &= ~mask;
+    board.whiteQueens  &= ~mask;
+    board.whiteKing    &= ~mask;
+
+    board.blackPawns   &= ~mask;
+    board.blackKnights &= ~mask;
+    board.blackBishops &= ~mask;
+    board.blackRooks   &= ~mask;
+    board.blackQueens  &= ~mask;
+    board.blackKing    &= ~mask;
+}
+
+void changeBoard(char move[]){
     int from = squareFromString(move[0], move[1]);
     int to   = squareFromString(move[2], move[3]);
 
     uint64_t fromMask = 1ULL << from;
     uint64_t toMask   = 1ULL << to;
 
+    if(occupied(&board, to))
+    {
+        capturePiece(to);
+    }
 
     // белые пешки
     if(board.whitePawns & fromMask)
@@ -281,8 +671,6 @@ void changeBoard(char move[])
         board.whiteKing |= toMask;
     }
 
-
-
     // черные пешки
     else if(board.blackPawns & fromMask)
     {
@@ -319,7 +707,6 @@ void changeBoard(char move[])
         board.blackKing &= ~fromMask;
         board.blackKing |= toMask;
     }
-
 
     // смена хода
     board.whiteTurn = !board.whiteTurn;
@@ -380,23 +767,10 @@ void initialize960(Board *b)
                 rook2=i;
         }
     }
-
-
-
-    b->whiteBishops =
-        (1ULL<<bishop1) |
-        (1ULL<<bishop2);
-
-
-    b->whiteQueens =
-        (1ULL<<queen);
-
-
-
+    b->whiteBishops = (1ULL<<bishop1) | (1ULL<<bishop2);
+    b->whiteQueens =(1ULL<<queen);
     // оставшиеся пустые были кони
-
     b->whiteKnights=0;
-
 
     for(int i=0;i<8;i++)
     {
@@ -409,24 +783,15 @@ void initialize960(Board *b)
         }
     }
 
-
-
     b->whiteRooks =
         (1ULL<<rook1) |
         (1ULL<<rook2);
 
-
     b->whiteKing =
         (1ULL<<king);
-
-
-
     // pawns
-
     b->whitePawns =
         0x000000000000FF00;
-
-
 
     // black mirror
 
@@ -434,27 +799,20 @@ void initialize960(Board *b)
     b->blackPawns =
         b->whitePawns<<40;
 
-
     b->blackRooks =
         b->whiteRooks<<56;
-
 
     b->blackKing =
         b->whiteKing<<56;
 
-
     b->blackQueens =
         b->whiteQueens<<56;
-
 
     b->blackBishops =
         b->whiteBishops<<56;
 
-
     b->blackKnights =
         b->whiteKnights<<56;
-
-
 
     b->whiteTurn=1;
     b->castlingRights=15;
@@ -522,51 +880,56 @@ void addMove(char move[])
 
 void sendPosition(FILE *engineOut)
 {
-    fprintf(engineOut, "position startpos");
+    fprintf(engineOut,"setoption name UCI_Chess960 value true\n");
 
-    if(moveCount > 0)
+    fprintf(engineOut,
+    "position fen %s",
+    startPosition);
+
+
+    if(moveCount>0)
     {
-        fprintf(engineOut, " moves");
+        fprintf(engineOut," moves ");
 
-        for(int i = 0; i < moveCount; i++)
+        for(int i=0;i<moveCount;i++)
         {
-            fprintf(engineOut, " %s", moveHistory[i]);
+            fprintf(engineOut,"%s ",
+            moveHistory[i]);
         }
     }
 
-    fprintf(engineOut, "\n");
-    fflush(engineOut);
+
+    fprintf(engineOut,"\n");
 }
 
-void pvp()
-{
-    bool isWhiteTurn = true;
+void pvp(){
     char move[6];
 
     while(!isGameOver(&board))
     {
         printBoard(&board);
-        if(isWhiteTurn)
+
+        if(board.whiteTurn)
             printf("\nWhite turn.\nMake your move: ");
         else
             printf("\nBlack turn.\nMake your move: ");
 
+
         scanf("%5s", move);
 
-        if(legitMove(move, isWhiteTurn))
+
+        if(legitMove(move, board.whiteTurn))
         {
             changeBoard(move);
-            isWhiteTurn = !isWhiteTurn;
         }
         else
         {
-            printf("\nError: move is not legit\n");
+            printf("Illegal move\n");
         }
     }
 }
 
-void pvb(int color)
-{
+void pvb(int color){
     printf("Bot mode\n");
 
     int to_engine[2];
@@ -669,10 +1032,13 @@ void pvb(int color)
 
                     printf("Stockfish plays: %s\n",move);
 
-                    changeBoard(move);
-
-                    addMove(move);
-
+                    if(legitMove(move, board.whiteTurn)){
+                        changeBoard(move);
+                        addMove(move);
+                    }
+                    else{
+                        printf("Chess engine prodused wrong move");
+                    }
                     break;
                 }
             }
@@ -686,8 +1052,7 @@ void pvb(int color)
     fclose(engineOut);
 }
 
-int main()
-{
+int main(){
     int gamemode;
     int startpos;
     int color;
@@ -746,6 +1111,7 @@ int main()
             break;
         case 2:
             initialize960(&board);
+            createFEN(&board);
             break;
         case 3:
             initializeClassic(&board);
